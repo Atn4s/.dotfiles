a8962594a1103437694e5e2d85188592018aef71
Atn4s <joaopsantaclara115@gmail.com>
2024-10-29 23:21:10 -0300

    v3.1 - refactoring

--- setup.sh
+++ setup.sh
@@ -1,47 +1,15 @@
 #!/bin/bash
 
+# Packages and tools 
 varApt=(
-    adb
-    cava
-    cowsay
-    cmatrix
-    extrepo
-    figlet
-    gimp
-    git
-    gparted
-    gsmartcontrol
-    guvcview
-    htop
-    iftop
-    keepassxc
-    kitty
-    krita
-    nmap
-    obs-studio 
-    openjdk-21-jdk
-    piper
-    python3-pip
-    python3-venv
-    ranger
-    rkhunter
-    scrcpy
-    sqlitebrowser
-    sqlite3
-    steghide
-    tty-clock
-    veracrypt
-    vrms
-    vlc
-    whois
-    xournalpp
+    adb cava cowsay cmatrix extrepo figlet gimp git gparted gsmartcontrol 
+    guvcview htop iftop keepassxc kitty krita nmap obs-studio openjdk-21-jdk 
+    piper python3-pip python3-venv ranger rkhunter scrcpy sqlitebrowser sqlite3 
+    steghide tty-clock veracrypt vrms vlc whois xournalpp
 )
 
 varFlatpak=(
-    com.getpostman.Postman
-    com.spotify.Client
-    net.opentabletdriver.OpenTabletDriver
-    org.apache.netbeans    
+    com.getpostman.Postman com.spotify.Client net.opentabletdriver.OpenTabletDriver org.apache.netbeans    
 )
 
 varPPAs=(
@@ -57,11 +25,10 @@ enable_librewolf(){
 
 AppImages(){
     echo "Remember to Download these AppImages:"
-    echo "Joplin"
-    echo "LocalSend"
-    echo "Obsidian"
+    echo "Joplin, LocalSend, Obsidian"
 }
 
+# Main code
 clear
 
 echo "    ___   __        __ __         ____        __  _____ __         "
@@ -71,34 +38,28 @@ echo " / ___ / /_/ / / /__  __(__  ) / /_/ / /_/ / /_/ __/ / /  __(__  ) "
 echo "/_/  |_\__/_/ /_/  /_/ /____(_)_____/\____/\__/_/ /_/_/\___/____/  "
 echo -e                                                                   
                                                                                           
-echo -e
-echo "> Adding PPA's to the system"
+echo -e "> Adding PPA's to the system"
 for ppa in "${varPPAs[@]}"; do
     sudo add-apt-repository "$ppa"
-    echo "TEST"
 done
 
-echo -e
-echo "> Update and Upgrade APT and Flatpak"
+echo -e "> Update and Upgrade APT and Flatpak"
 sudo apt update && sudo apt upgrade && flatpak upgrade
 
-echo -e
-echo "> Installing Apt programs"
+echo -e "> Installing Apt programs"
 sudo apt install "${varApt[@]}"
 enable_librewolf
 
-echo -e
-echo "> Installing Flatpak programs"
+echo -e "> Installing Flatpak programs"
 flatpak install "${varFlatpak[@]}"                                                                    
                           
-echo "> Adding my scripts"               
+echo -e "> Adding my scripts"               
 
 git clone https://github.com/Atn4s/scava.sh.git
 git clone https://github.com/Atn4s/NoSpyCam.git
 
 mv .bash_aliases $HOME
-echo "> .bash_aliases added"
+echo -e "> .bash_aliases added"
 
 AppImages
-echo -e
-echo "[ Your Distro is ready to go! ]"
+echo -e "[ Your Distro is ready to go! ]"
