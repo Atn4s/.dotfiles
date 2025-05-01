e7b25d6a9e789fb92dad9e6cfff960eccc2f0eb3
Atn4s <joaopsantaclara115@gmail.com>
2025-05-01 00:23:06 -0300

    v 4.1.0 - change apps, new wallpapers

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Packages and tools 
+# ----------------- CONFIGURAÇÕES -----------------
+
+# Lista de pacotes APT
 varApt=(
     adb cava cowsay cmatrix extrepo fastfetch figlet gimp git gparted gsmartcontrol 
     guvcview htop iftop keepassxc kitty krita neovim nmap obs-studio openjdk-21-jdk 
@@ -8,67 +10,96 @@ varApt=(
     steghide syncthing syncthing-gtk tty-clock veracrypt vrms vlc whois xournalpp
 )
 
+# Lista de pacotes Flatpak
 varFlatpak=(
-    com.getpostman.Postman com.spotify.Client md.obsidian.Obsidian net.mullvad.MullvadBrowser net.opentabletdriver.OpenTabletDriver org.apache.netbeans
+    com.github.wwmm.easyeffects com.jetbrains.IntelliJ-IDEA-Ultimate net.cozic.joplin_desktop
+    net.opentabletdriver.OpenTabletDriver 
 )
 
+# PPAs a serem adicionadas
 varPPAs=(
+    "ppa:obsproject/obs-studio"
     "ppa:phoerious/keepassxc"
     "ppa:unit193/encryption"
     "ppa:zhangsongcui3371/fastfetch"
 )
 
-enable_extrepo(){
-    sudo extrepo update
+# ----------------- FUNÇÕES -----------------
+
+header(){
+    clear
+    echo -e "\e[1;34m"
+    echo "    ___   __        __ __         ____        __  _____ __         "
+    echo "   /   | / /_____  / // / _____  / __ \____  / /_/ __(_) /__  _____"
+    echo "  / /| |/ __/ __ \/ // /_/ ___/ / / / / __ \/ __/ /_/ / / _ \/ ___/"
+    echo " / ___ / /_/ / / /__  __(__  ) / /_/ / /_/ / /_/ __/ / /  __(__  ) "
+    echo "/_/  |_\__/_/ /_/  /_/ /____(_)_____/\____/\__/_/ /_/_/\___/____/  "
+    echo -e                                                                   
 }
 
-PythonImage(){
+# Adiciona os PPAs
+add_ppas(){
+    echo -e "\e[1;32m> Adicionando PPAs...\e[0m"
+    for ppa in "${PPAS[@]}"; do
+        sudo add-apt-repository -y "$ppa" && echo "✔ PPA $ppa adicionada com sucesso!"
+    done
+}
+
+# Atualização e Upgrade de pacotes
+update_system(){
+    echo -e "\e[1;32m> Atualizando o sistema...\e[0m"
+    sudo apt update && sudo apt upgrade -y 
+    flatpak upgrade -y
+}
+
+#Instalação de pacotes APT
+install_apt_packages(){
+    echo -e "\e[1;32m> Instalando pacotes APT...\e[0m"
+    sudo apt install "${varApt[@]}"
+}
+
+# Habilitando repositório extrepo
+enable_extrepo(){
+    echo -e "\e[1;32m> Habilitando repositórios externos...\e[0m"
+    sudo extrepo update
+    sudo extrepo enable librewolf
+    sudo apt update && sudo apt install librewolf
+}
+
+install_flatpak_packages(){
+    echo -e "\e[1;32m> Instalando pacotes Flatpak...\e[0m"
+    flatpak install "${varFlatpak[@]}"      
+}
+
+setup_pywal(){
+    echo -e "\e[1;32m> Configurando PyWal...\e[0m"
     python3 -m venv $HOME/wal_venv/
     source $HOME/wal_venv/bin/activate
     pip install pywal
     deactivate
 }
+                                 
+git_clone(){
+    echo -e "\e[1;32m> Clonando outros repositórios\e[0m"    
+    git clone https://github.com/Atn4s/scava.sh.git
+    git clone https://github.com/Atn4s/NoSpyCam.git
+    git clone https://github.com/Atn4s/Daemon-HubUSB.C.git
 
-# Main code
-clear
+    mv .bash_aliases $HOME
+    echo "✔ .bash_aliases adicionado!"
 
-echo "    ___   __        __ __         ____        __  _____ __         "
-echo "   /   | / /_____  / // / _____  / __ \____  / /_/ __(_) /__  _____"
-echo "  / /| |/ __/ __ \/ // /_/ ___/ / / / / __ \/ __/ /_/ / / _ \/ ___/"
-echo " / ___ / /_/ / / /__  __(__  ) / /_/ / /_/ / /_/ __/ / /  __(__  ) "
-echo "/_/  |_\__/_/ /_/  /_/ /____(_)_____/\____/\__/_/ /_/_/\___/____/  "
-echo -e                                                                   
-                                                                                          
-echo -e "> Adding PPA's to the system"
-for ppa in "${varPPAs[@]}"; do
-    sudo add-apt-repository "$ppa"
-done
+    cp -r .config/* $HOME/.config/
+    echo "✔ Arquivos de configuração adicionados!"
+}
 
-echo -e "> Update and Upgrade APT and Flatpak"
-sudo apt update && sudo apt upgrade && flatpak upgrade
+# ----------------- EXECUÇÃO -----------------
 
-echo -e "> Installing Apt programs"
-sudo apt install "${varApt[@]}"
-
+header
+add_ppas
+update_system
 enable_extrepo
-
-echo -e "> Installing Flatpak programs"
-flatpak install "${varFlatpak[@]}"                                                                    
-                          
-echo -e "> Adding my scripts"               
-
-git clone https://github.com/Atn4s/scava.sh.git
-git clone https://github.com/Atn4s/NoSpyCam.git
-git clone https://github.com/Atn4s/Daemon-HubUSB.C.git
-
-mv .bash_aliases $HOME
-echo -e "> .bash_aliases added"
-
-mv .config/ranger $HOME/.config
-
-cp -r .config/ ~/
-echo -e "> .config files are now added to the system"
-
-PythonImage
-
-echo -e "[ Your Distro is ready to go! ]"
+install_apt_packages
+install_flatpak_packages
+setup_pywal
+git_clone                     
+echo -e "\e[1;32m✔ Seu sistema está pronto para uso!\e[0m"
