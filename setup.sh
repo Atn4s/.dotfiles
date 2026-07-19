9280f9ceb8a51a4fd8a84ab62aed2497ee52c460
Atn4s <joaopsantaclara115@gmail.com>
2026-07-18 22:43:09 -0300

    Add: docker installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -72,6 +72,33 @@ add_ppas(){
     done
 }
 
+# Docker instalação:
+install_docker(){
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
+}
+
 # Atualização e Upgrade de pacotes
 update_system(){
     msg "Atualizando sistema"
@@ -154,7 +181,7 @@ git_clone(){
     git clone https://github.com/Atn4s/NoSpyCam.git || true
     git clone https://github.com/Atn4s/Daemon-HubUSB.C.git || true
 
-    [[ -f .bash_aliases ]] && mv .bash_aliases "$HOME"
+    [[ -f .bash_aliases ]] && cp .bash_aliases "$HOME"
     [[ -d .config ]] && cp -r .config/* "$HOME/.config/"
 
     msg "Repositórios clonados em $PROJECTS_DIR"
@@ -166,6 +193,7 @@ git_clone(){
 header
 add_ppas
 vscode
+install_docker
 enable_extrepo
 update_system
 install_apt_packages
